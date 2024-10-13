9b11bfef81e62dda2e9511d4c62eff5f1152d00c
sangarananthan <sangarsmart16@gmail.com>
2024-10-13 18:42:53 +0530

    logout controller addded

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -52,4 +52,12 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+const logoutCurrentUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out successfully" });
+});
+
 export { createUser, loginUser, logoutCurrentUser };
